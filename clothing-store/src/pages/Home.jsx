a9dd0e5ee478c03520f6ffbac4d0367438b47275
waizin28 <waizinlinn.08@gmail.com>
2024-12-03 00:44:08 -0600

    reedited NewsLetterBox

--- clothing-store/src/pages/Home.jsx
+++ clothing-store/src/pages/Home.jsx
@@ -4,6 +4,7 @@ import LatestCollection from '../components/LatestCollection';
 import BestSeller from '../components/BestSeller';
 import OurPolicy from '../components/OurPolicy';
 import NewsLetterBox from '../components/NewsLetterBox';
+
 const Home = () => {
   return (
     <div>
